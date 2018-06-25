07e38738a434433335181ae55336eba0b731740d
Muzzi Aldean <amiraldean@gmail.com>
2018-06-25 18:12:33 +0100

    Created isDivisibleByThree function

---
+++ FizzBuzz/Brain.swift
@@ -0,0 +1,15 @@
+//
+//  Brain.swift
+//  FizzBuzz
+//
+//  Created by Muzzi Aldean on 25/06/2018.
+//  Copyright © 2018 Muzzi Aldean. All rights reserved.
+//
+
+import Foundation
+
+class Brain {
+    func isDivisibleByThree(number: Int) -> Bool {
+        return true
+    }
+}
